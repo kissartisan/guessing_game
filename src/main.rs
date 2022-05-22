5baedaf4180650e3aa9167bc6e370bd2c5b41977
Reymark <reymark.torres08.dev@gmail.com>
2022-05-22 16:20:14 +0800

    Limit the number of tries to 5

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ fn main() {
     
     // You can do 1..=100 as well as a range from 1 to 100
     let secret_number = rand::thread_rng().gen_range(1..101);
-    
+    let mut allowed_tries = 5;
+
     loop {
-        println!("Please input your guess.");
+        println!("Please input your guess:");
 
         let mut guess = String::new();
         
@@ -22,15 +23,28 @@ fn main() {
             Err(_) => continue,
         };
 
+        println!("============================");
         println!("You guessed: {}", guess);
+
+        
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Greater => println!("Too big!"), 
             Ordering::Equal => {
                 println!("You win!");
                 break;
             }
+        }
+       
+        allowed_tries-=1;
+        
+        println!("Tries left: {}", allowed_tries);
+        println!("============================");
+
+        if allowed_tries == 0 {
+            println!("No more tries left. You lose!");
+            break;
         }
     }
 }
