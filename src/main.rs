709a6a7b99640b5944580fe90f21cb5bfbc3452b
Reymark <reymark.torres08.dev@gmail.com>
2022-05-22 14:28:48 +0800

    Generating a Random Number

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    
+    // You can do 1..=100 as well as a range from 1 to 100
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    
+    println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
-    let mut guess = String::new();
 
+    let mut guess = String::new();
+    
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
